1985271570761e4dc5841db02ac8f1842f215307
Dikshit Kaushal <47450978+dikshitkaushal@users.noreply.github.com>
2020-07-07 12:17:35 +0530

    first_commit

---
+++ Ping_Pong_Ball/Assets/MoveBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour {
+
+	Vector3 startposition;
+	float speed = 10f;
+
+
+	// Use this for initialization
+	void Start () {
+		startposition = this.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
